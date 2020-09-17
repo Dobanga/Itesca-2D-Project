69d69bcec0ddf56e8eb7f3193177439beb5b4bb5
carlossaidlopez <69657298+carlossaidlopez@users.noreply.github.com>
2020-09-17 05:22:07 -0700

    Update Player.cs
    Co-Authored-By: Paola Herrera <43833851+payolaherrera@users.noreply.github.com>

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -14,11 +17,20 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     ContactFilter2D groundFilter;
-
+    
     Animator anim;
     
     [SerializeField]
     GameObject Sonido;
+
+    [SerializeField]
+    GameObject candado;
+
+    [SerializeField]
+    Text MensajeCondicion;
+
+
+    bool MC=false;
 
     void Awake()
     {
@@ -44,7 +56,22 @@ public class Player : MonoBehaviour
 
     //cosas normales
     void Update()
-    {
+    {   
+
+        if(Corazones.instance.VerCorazones()<0){
+            print("Fin del juego");
+            SceneManager.LoadScene("Menu");
+
+        }
+
+        if(Score.instance.VerScore()> 23 ){
+            print("puede terminar");
+            Destroy(candado,1f);
+            Destroy(MensajeCondicion);
+            MC=true;
+        }
+
+
         if(IsMoving)
             if(Axis.x > 0)
                 transform.localRotation = Quaternion.Euler(0,136,0);
@@ -83,6 +110,41 @@ public class Player : MonoBehaviour
             rb2D.AddForce(JumpDir, ForceMode2D.Impulse);
         }
     }
+
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Gemas")){
+         
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.CompareTag("ZonaMensaje")){//si entra a la zona de mensaje 
+            if(MC==false){
+            MensajeCondicion.text="Necesita al menos 24 Gemas";
+             TestCorrect();
+            }
+           
+             
+        }
+
+        if(other.gameObject.CompareTag("GemaNivel")){//si toma la Gema para pasar de nivel
+            SceneManager.LoadScene("Menu");
+            print("Pasa nivel");
+
+        }
+        
+    }
+
+    public async Task TestCorrect() //Delay para mensaje de las gemas necesarias
+    {
+        await Task.Factory.StartNew(async () =>
+        {
+            await Task.Delay(5000);
+        }).Unwrap(); 
+         MensajeCondicion.text="";
+        ;
+    }
+
 
     Vector2 DirectionX => Vector2.right * Axis.x * moveSpeed;
 
